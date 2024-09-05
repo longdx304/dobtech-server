6011fe6468c9f728aab712f33da25f55dad37929
thienduyy <nguyenvthienduy@gmail.com>
2024-09-05 15:08:04 +0700

    feat: extend column supplier_price for table product variant

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,16 +1,24 @@
 import { registerOverriddenValidators } from '@medusajs/medusa';
 import {
-  StorePostCartReq,
-  StorePostCartsCartReq,
+	StorePostCartReq,
+	StorePostCartsCartReq,
 } from '../extend-validator/cart';
 import {
-  AdminCreateUserRequest,
-  AdminUpdateUserRequest,
+	AdminCreateUserRequest,
+	AdminUpdateUserRequest,
 } from '../extend-validator/user';
 import { registerExtendedValidator } from '../utils/register-exntended-validator';
+import {
+	AdminPostProductsProductVariantsReq,
+	AdminPostProductsProductVariantsVariantReq,
+} from '../extend-validator/product';
 
 registerOverriddenValidators(AdminCreateUserRequest);
 registerOverriddenValidators(AdminUpdateUserRequest);
 
 registerExtendedValidator(StorePostCartsCartReq);
 registerExtendedValidator(StorePostCartReq);
+
+// Register extended validators for product variants
+registerExtendedValidator(AdminPostProductsProductVariantsReq);
+registerExtendedValidator(AdminPostProductsProductVariantsVariantReq);
