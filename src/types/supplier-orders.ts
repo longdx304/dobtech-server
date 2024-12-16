4875bcf40e25a01b3b531ab13f55e070a4ea94f0
Nam Nguyen <ducnam1719@gmail.com>
2024-12-16 16:48:27 +0700

    fix: update api for list and count for supplier order with filter

--- src/types/supplier-orders.ts
+++ src/types/supplier-orders.ts
@@ -1,3 +1,18 @@
+import { DateComparisonOperator, IsType } from '@medusajs/medusa';
+import { Transform, Type } from 'class-transformer';
+import {
+	IsArray,
+	IsEnum,
+	IsOptional,
+	IsString,
+	ValidateNested,
+} from 'class-validator';
+import {
+	FulfillSupplierOrderStt,
+	OrderStatus,
+	PaymentStatus,
+} from '../models/supplier-order';
+
 export type LineItem = {
 	variantId: string;
 	quantity: number;
@@ -46,4 +61,139 @@ export type SupplierOrderSelector = {
 
 export interface DeleteLineItemRequest {
 	lineItemId: string;
+}
+
+/**
+ * Filters to apply on the retrieved orders.
+ */
+export class AdminListSupplierOrdersSelector {
+	/**
+	 * Search term to search orders' shipping address, first name, email, and display ID.
+	 */
+	@IsString()
+	@IsOptional()
+	q?: string;
+
+	/**
+	 * ID to filter orders by.
+	 */
+	@IsString()
+	@IsOptional()
+	id?: string;
+
+	/**
+	 * Statuses to filter orders by.
+	 */
+	@IsArray()
+	@IsEnum(OrderStatus, { each: true })
+	@IsOptional()
+	@Transform(({ value }) => {
+		if (typeof value === 'string') {
+			return value.split(',').map((v) => v.trim());
+		}
+		return value;
+	})
+	status?: string[];
+
+	/**
+	 * Fulfillment statuses to filter orders by.
+	 */
+	@IsArray()
+	@IsEnum(FulfillSupplierOrderStt, { each: true })
+	@IsOptional()
+	@Transform(({ value }) => {
+		if (typeof value === 'string') {
+			return value.split(',').map((v) => v.trim());
+		}
+		return value;
+	})
+	fulfillment_status?: string[];
+
+	/**
+	 * Payment statuses to filter orders by.
+	 */
+	@IsArray()
+	@IsEnum(PaymentStatus, { each: true })
+	@IsOptional()
+	payment_status?: string[];
+
+	/**
+	 * Display ID to filter orders by.
+	 */
+	@IsString()
+	@IsOptional()
+	display_id?: string;
+
+	/**
+	 * Cart ID to filter orders by.
+	 */
+	@IsString()
+	@IsOptional()
+	cart_id?: string;
+
+	/**
+	 * Customer ID to filter orders by.
+	 */
+	@IsString()
+	@IsOptional()
+	customer_id?: string;
+
+	/**
+	 * Email to filter orders by.
+	 */
+	@IsString()
+	@IsOptional()
+	email?: string;
+
+	/**
+	 * Regions to filter orders by.
+	 */
+	@IsOptional()
+	@IsType([String, [String]])
+	region_id?: string | string[];
+
+	/**
+	 * Currency code to filter orders by.
+	 */
+	@IsString()
+	@IsOptional()
+	currency_code?: string;
+
+	/**
+	 * Tax rate to filter orders by.
+	 */
+	@IsString()
+	@IsOptional()
+	tax_rate?: string;
+
+	/**
+	 * Sales channel IDs to filter orders by.
+	 */
+	@IsArray()
+	@IsOptional()
+	sales_channel_id?: string[];
+
+	/**
+	 * Date filters to apply on the orders' `canceled_at` date.
+	 */
+	@IsOptional()
+	@ValidateNested()
+	@Type(() => DateComparisonOperator)
+	canceled_at?: DateComparisonOperator;
+
+	/**
+	 * Date filters to apply on the orders' `created_at` date.
+	 */
+	@IsOptional()
+	@ValidateNested()
+	@Type(() => DateComparisonOperator)
+	created_at?: DateComparisonOperator;
+
+	/**
+	 * Date filters to apply on the orders' `updated_at` date.
+	 */
+	@IsOptional()
+	@ValidateNested()
+	@Type(() => DateComparisonOperator)
+	updated_at?: DateComparisonOperator;
 }
