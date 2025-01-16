c1eae72f7c7cbb4778bc8af54e009b9404736e2a
thienduyy <nguyenvthienduy@gmail.com>
2025-01-16 15:34:27 +0700

    feature: async firebase inventory warehouse

--- src/services/warehouse.ts
+++ src/services/warehouse.ts
@@ -15,29 +15,34 @@ import {
 } from 'src/types/warehouse';
 import { EntityManager, FindOptionsWhere, ILike } from 'typeorm';
 import WarehouseInventoryService from './warehouse-inventory';
+import ItemUnitService from './item-unit';
 
 type InjectedDependencies = {
 	manager: EntityManager;
 	warehouseRepository: typeof WarehouseRepository;
 	warehouseInventoryRepository: typeof WarehouseInventoryRepository;
 	warehouseInventoryService: WarehouseInventoryService;
+	itemUnitService: ItemUnitService;
 };
 
 class WarehouseService extends TransactionBaseService {
 	protected warehouseRepository_: typeof WarehouseRepository;
 	protected warehouseInventoryRepository_: typeof WarehouseInventoryRepository;
 	protected warehouseInventoryService_: WarehouseInventoryService;
+	protected itemUnitService_: ItemUnitService;
 
 	constructor({
 		warehouseRepository,
 		warehouseInventoryRepository,
 		warehouseInventoryService,
+		itemUnitService,
 	}: InjectedDependencies) {
 		super(arguments[0]);
 
 		this.warehouseRepository_ = warehouseRepository;
 		this.warehouseInventoryRepository_ = warehouseInventoryRepository;
 		this.warehouseInventoryService_ = warehouseInventoryService;
+		this.itemUnitService_ = itemUnitService;
 	}
 
 	async listAndCount(
@@ -251,6 +256,82 @@ class WarehouseService extends TransactionBaseService {
 			);
 		}
 		await warehouseRepo.delete(id);
+	}
+
+	async updateWarehouseWithVariantFromFire(payload: Record<string, any>) {
+		return this.atomicPhase_(async (manager: EntityManager) => {
+			const warehouseInventoryServiceTx =
+				this.warehouseInventoryService_.withTransaction(manager);
+
+			const itemUnitServiceTx = this.itemUnitService_.withTransaction(manager);
+
+			const warehouseRepo = manager.withRepository(this.warehouseRepository_);
+			const warehouseInventoryRepo = manager.withRepository(
+				this.warehouseInventoryRepository_
+			);
+
+			const warehouse = await warehouseRepo.find();
+			const itemUnits = await itemUnitServiceTx.list();
+
+			const warehouseInventory = await warehouseInventoryRepo.find();
+
+			const warehouseInventoryMap = new Map();
+			const itemUnitMap = new Map();
+
+			warehouseInventory.forEach((item) => {
+				const key = `${item.warehouse_id}_${item.variant_id}_${item.unit_id}`;
+				warehouseInventoryMap.set(key, item);
+			});
+
+			itemUnits.forEach((item) => {
+				itemUnitMap.set(item.unit, item);
+			});
+
+			for (const key in payload) {
+				// Check key is warehouse location
+				const isWarehouseExist = warehouse.find(
+					(item) => item.location === key
+				);
+				const warehouseData = payload[key];
+
+				// Find the warehouse location if not exist, create new warehouse
+				let warehouseId = null;
+				if (!isWarehouseExist) {
+					const newWarehouse = await warehouseRepo.create({ location: key });
+					const warehouse = await warehouseRepo.save(newWarehouse);
+					warehouseId = warehouse.id;
+				} else {
+					warehouseId = isWarehouseExist.id;
+				}
+
+				// Loop through warehouse data
+				for (const item of warehouseData) {
+					const unit = itemUnitMap.get(item.unit);
+					if (!unit) {
+						continue;
+					}
+					const key = `${warehouseId}_${item.variant_id}_${unit.id}`;
+					const warehouseInventory = warehouseInventoryMap.get(key);
+
+					if (warehouseInventory) {
+						if (warehouseInventory.quantity !== item.quantity * unit.quantity) {
+							// Update warehouse inventory
+							await warehouseInventoryServiceTx.update(warehouseInventory.id, {
+								quantity: item.quantity * unit.quantity,
+							});
+						}
+					} else {
+						// Create new warehouse inventory
+						await warehouseInventoryServiceTx.create({
+							warehouse_id: warehouseId,
+							variant_id: item.variant_id,
+							unit_id: unit.id,
+							quantity: item.quantity * unit.quantity,
+						});
+					}
+				}
+			}
+		});
 	}
 }
 
